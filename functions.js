b59662f60b2551dd3dd840d21478239716e79c7d
joao-gabriel-fekete <jgfekete@gmail.com>
2022-02-28 15:46:17 -0300

    Coffee four

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
 function makeCoffee(){
-    window.open("coffee.html", 'popUpWindow', 'height=560', 'width=480')
+    window.open("coffee.html", 'popUpWindow','height=560,width=480,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
 }
 
